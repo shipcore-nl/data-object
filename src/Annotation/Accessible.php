5bd45277b93f66cd10ba0106f73220b35bb61b7c
niccifor <niccifor@gmail.com>
2017-10-21 14:12:03 +0200

    Extract data object code from dhl api repository

---
+++ src/Annotation/Accessible.php
@@ -0,0 +1,12 @@
+<?php
+namespace ShipCore\DataObject\Annotation;
+
+use Doctrine\Common\Annotations\Annotation;
+
+/**
+ * @Annotation()
+ * @Annotation\Target("PROPERTY")
+ */
+class Accessible
+{
+}
---
+++ src/Exception/MissingPropertyException.php
@@ -0,0 +1,25 @@
+<?php
+namespace ShipCore\DataObject\Exception;
+
+class MissingPropertyException extends \Exception
+{
+    /**
+     *
+     * @var string
+     */
+    private $className;
+    
+    /**
+     *
+     * @var string
+     */
+    private $propertyName;
+    
+    public function __construct($className, $propertyName, \Throwable $previous = null)
+    {
+        $this->className = $className;
+        $this->propertyName = $propertyName;
+        $message = "$className::$$propertyName is required.";
+        parent::__construct($message, 0, $previous);
+    }
+}
